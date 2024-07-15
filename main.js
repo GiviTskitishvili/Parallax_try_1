a0bae24a3481190d19db4a9da2d607223a4e0b60
GiviTskitishvili <tgivi@yahoo.com>
2024-07-16 00:56:46 +0400

    updated gsap and imgs

--- main.js
+++ main.js
@@ -1,58 +1,29 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
+  gsap.registerPlugin(ScrollTrigger);
 
-    let controller = new ScrollMagic.Controller();
+  let timeline = gsap.timeline({
+    scrollTrigger: {
+      trigger: "section",
+      start: "top top",
+      end: "+=200%",
+      scrub: true,
+      pin: true,
+    },
+  });
 
-    let timeline = new TimelineMax()
-
-    timeline
-        .to('#sixth', 6, {
-            y: -700
-        })
-        .to('#fifth', 6, {
-            y: -500
-        }, '-=6')
-        .to('#forth', 6, {
-            y: -400
-        }, '-=6')
-        .to('#third', 6, {
-            y: -300
-        }, '-=6')
-        .to('#second', 6, {
-            y: -200
-        }, '-=6')
-        .to('#first', 6, {
-            y: -100
-        }, '-=6')
-        .to('.content, .blur', 6, {
-            top: '0%'
-        }, '-=6')
-        .to('.title, nav, .footer-wrapper', 6, {
-            y: -600
-        }, '-=6')
-        .from('.one', 3, {
-            top: '40px',
-            autoAlpha: 0
-        }, '-=4')
-        .from('.two', 3, {
-            top: '40px',
-            autoAlpha: 0
-        }, '-=3.5')
-        .from('.three', 3, {
-            top: '40px',
-            autoAlpha: 0
-        }, '-=3.5')
-        .from('.four', 3, {
-            top: '40px',
-            autoAlpha: 0
-        }, '-=4')
-
-
-    let scene = new ScrollMagic.Scene({
-        triggerElement: 'section',
-        duration: '200%',
-        triggerHook: 0
-    })
-        .setTween(timeline)
-        .setPin('section')
-        .addTo(controller)
-})
+  timeline
+    // .to(".title", { y: -200, duration: 3 })
+    .to("#sixth", { y: -700, duration: 6 })
+    .to("#fifth", { y: -500, duration: 6 }, "-=6")
+    .to("#forth", { y: -400, duration: 6 }, "-=6")
+    .to("#third", { y: -300, duration: 6 }, "-=6")
+    .to("#second", { y: -200, duration: 6 }, "-=6")
+    .to("#first", { y: -100, duration: 6 }, "-=6")
+    .to(".content, .blur", { top: "0%", duration: 6 }, "-=6")
+    .to(".title, nav, .footer-wrapper", { y: -600, duration: 6 }, "-=6")
+    .from(".title", { top: "350px", autoAlpha: 1, duration: 3 }, "-=5")
+    .from(".one", { top: "40px", autoAlpha: 0, duration: 3 }, "-=4")
+    .from(".two", { top: "40px", autoAlpha: 0, duration: 3 }, "-=3.5")
+    .from(".three", { top: "40px", autoAlpha: 0, duration: 3 }, "-=3.5")
+    .from(".four", { top: "40px", autoAlpha: 0, duration: 3 }, "-=4");
+});
